7b7d5c7db65fdb8d8f10959a23eef112240b3cdf
gatimu82 <simongatimu82@gmail.com>
2022-09-01 12:06:39 +0300

    secondcommit

--- app/src/main/java/com/example/simonsshop/Myhome.kt
+++ app/src/main/java/com/example/simonsshop/Myhome.kt
@@ -4,19 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class Myhome : AppCompatActivity() {
     lateinit var ordernow:Button
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var logout:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_myhome)
 
         supportActionBar?.hide()
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        logout = findViewById(R.id.logoutbtn)
         ordernow = findViewById(R.id.mBtnOrdernow)
         ordernow.setOnClickListener {
             intent = Intent(this, shop::class.java)
             startActivity(intent)
+        }
+
+        logout.setOnClickListener {
+            firebaseAuth.signOut()
+            startActivity(Intent(this, MainActivity::class.java))
+
         }
 
     }
